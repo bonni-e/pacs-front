61a8fb656894d3bdd845c22ab4b9d35fcde7a97c
bonni-e <trial1bloom@gmail.com>
2023-11-03 18:08:23 +0900

    Modify Footer height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 export default function Footer() {
     return (
-        <Box w={'100%'} h={'150px'} bg={'blackAlpha.100'} padding={'23px'} mt={'30px'}>
+        <Box w={'100%'} h={'200px'} bg={'blackAlpha.100'} padding={'23px'} mt={'30px'}>
             <Text color={'gray.600'}>
                 address
             </Text>
