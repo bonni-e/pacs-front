b15508d3b75ac89ae44b7c1848ced6745a68e1f8
bonni-e <trial1bloom@gmail.com>
2023-11-06 16:24:32 +0900

    Add useState : pid, pname, reportstatus

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,9 +8,14 @@ export default function SearchBar() {
     const [pageSize, setPageSize] = useState(5);
     const [totalPageCount, setTotalPageCount] = useState(1);
 
+    const [pid, setPid] = useState('');
+    const [pname, setPname] = useState('');
+    const [reportstatus, setReportstatus] = useState(0);
+
     // Fetching
     const [isLoading, setIsLoading] = useState(true);
     const [studies, setStudies] = useState([]);
+
     const fetchStudies = async () => {
         const response = await fetch(`https://192.168.30.88:8443/v1/api/pacs/studies?page=${page}&pageSize=${pageSize}`, { method: 'GET' });
         const json = await response.json();
@@ -18,9 +23,28 @@ export default function SearchBar() {
         setTotalPageCount(json.totalPageCount);
         setIsLoading(false);
     }
+
+    const fetchSortStudies = async () => {
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        const data = {
+            "pid": pid,
+            "pname": pname,
+            "reportstatus": reportstatus
+        };
+        const response = await fetch(`https://192.168.30.88:8443/v1/api/pacs/studies?page=${page}&pageSize=${pageSize}`, { method: 'POST', headers: headers, body: JSON.stringify(data) });
+        const json = await response.json();
+        setStudies(json.list);
+        setTotalPageCount(json.totalPageCount);
+        setIsLoading(false);
+    }
+
     useEffect(() => {
-        fetchStudies();
-    }, [page, pageSize]);
+        if (pid !== '' || pname !== '' || reportstatus !== 0)
+            fetchSortStudies();
+        else
+            fetchStudies();
+    }, [pid, pname, reportstatus, page, pageSize]);
 
     function changePageSize(e: React.ChangeEvent<HTMLSelectElement>) {
         const pageSizeValue = parseInt(e.target.value);
@@ -29,23 +53,35 @@ export default function SearchBar() {
     }
 
     function changePagePrevious() {
-        if(page > 1)
+        if (page > 1)
             setPage(page - 1);
     }
 
     function changePageNext() {
-        if(page < totalPageCount)
+        if (page < totalPageCount)
             setPage(page + 1);
+    }
+
+    function sortPageWithPid(e: React.ChangeEvent<HTMLInputElement>) {
+        setPid(e.target.value);
+    }
+
+    function sortPageWithPname(e: React.ChangeEvent<HTMLInputElement>) {
+        setPname(e.target.value);
+    }
+
+    function sortPageWithReportstatus(e: React.ChangeEvent<HTMLSelectElement>) {
+        setReportstatus(parseInt(e.target.value));
     }
 
     return (
         <>
             <Box margin={'23px'}>
                 <HStack >
-                    <Input variant={'filled'} placeholder="환자아이디" />
-                    <Input variant={'filled'} placeholder="환자이름" />
-                    <Select variant={'filled'}>
-                        <option value={''} selected disabled>판독상태</option>
+                    <Input onChange={sortPageWithPid} variant={'filled'} placeholder="환자아이디" />
+                    <Input onChange={sortPageWithPname} variant={'filled'} placeholder="환자이름" />
+                    <Select onChange={sortPageWithReportstatus} variant={'filled'}>
+                        <option value={''} selected>판독상태</option>
                         <option value={'3'} >읽지않음</option>
                         <option value={'4'} >열람중</option>
                         <option value={'5'} >예비판독</option>
