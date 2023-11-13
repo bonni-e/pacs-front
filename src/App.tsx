11b3d3ddf97850e0c72f20bb6c0bff0b78972f45
bonni-e <trial1bloom@gmail.com>
2023-11-13 11:33:57 +0900

    Modify 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import 'styled-components';
 import Router from './Router';
 import { ChakraProvider } from '@chakra-ui/react';
 import Header from './components/Header';
-import Footer from './components/Footer';
+import Footer from './components/Footer'; 
 
 function App() {
   return (
