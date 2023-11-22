50e6a4f50a11b2ff53aec248818c96c4ef84a7a0
bonni-e <trial1bloom@gmail.com>
2023-11-22 11:26:07 +0900

    Remove React.StrictMode Tag

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // UseEffct 두 번 실행 문제 
+  // https://velog.io/@gene028/%EA%B0%9C%EB%B0%9C%EC%9D%BC%EC%A7%80-3-UseEffect%EB%8A%94-%EC%99%9C-%EB%91%90%EB%B2%88-%EC%8B%A4%ED%96%89%EB%90%98%EB%8A%94%EA%B1%B8%EA%B9%8C
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
